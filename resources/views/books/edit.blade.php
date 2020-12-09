f96303215908a706617eaf2f07ebeee3b7ad29d4
Basel Alebrahim <basselalibrahim@gmail.com>
2020-12-09 23:47:17 +0900

    feat: create view for editing a book (HTML, CSS)

---
+++ resources/views/books/edit.blade.php
@@ -0,0 +1,45 @@
+
+@extends('layouts.master')
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            <h1><span class="badge badge-info">Edit Book</span></h1>
+        </div>
+
+        <br>
+
+        @if (Session::has('success'))
+        <div class="alert alert-success" role="alert">
+            {{Session::get('success')}}
+        </div>
+        @endif
+
+        <br>
+
+        <form method="POST" action="{{route('books.update', $book->id)}}">
+            @csrf
+            <div class="form-group">
+                <label for="name">Book Title <span class="text-danger">*</span></label>
+                <input type="text" class="form-control col-sm-4" name="title" id="title" value="{{$book->title}}">
+                @error('title')
+                <small id="nameHelp" class="form-text text-danger">{{$message}}</small>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Select Author <span class="text-danger">*</span></label>
+                <select class="form-control col-sm-4" name="author_id" id="author_id">
+                    <option value="" selected>-- Select Author --</option>
+                    @foreach($authors as $author)
+                    <option  value= "{{ $author->id }}" {{$book->author->id == $author->id  ? 'selected' : ''}}>{{$author->name}}</option>
+                    @endforeach
+                </select>
+                @error('author_id')
+                <small id="nameHelp" class="form-text text-danger">{{$message}}</small>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+    </div>
+</div>
+@endsection
