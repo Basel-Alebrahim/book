5ca36f8cb84f8f9c8efab89d3b6dbc222a192969
Basel Alebrahim <basselalibrahim@gmail.com>
2020-12-09 23:14:14 +0900

    feat: create author seeder

---
+++ database/seeds/AuthorTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Author;
+
+class AuthorTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Author::create([
+            'name' => 'Basel',
+        ]);
+
+        factory(Author::class, 10)->create();
+    }
+}
